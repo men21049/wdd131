4add86e1e9493698a593020cece7bc1b44df5227
men21049 <men21049@byui.edu>
2026-01-06 21:13:34 -0600

    w1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,4 @@
+let currentYear = new Date().getFullYear();
+document.querySelector("#currentyear").innerHTML = `&copy; ${currentYear}`;
 let oLastModif = new Date(document.lastModified);
 document.querySelector("#lastmodified").innerHTML = oLastModif;
