cb1ca635b4141ddb9dcf058d258ddfba2f989677
men21049 <men21049@byui.edu>
2026-01-08 11:22:41 -0600

    w2

---
+++ week02/scripts/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if(input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        input.value = '';
+        input.focus();
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+    }
+  }
+);
