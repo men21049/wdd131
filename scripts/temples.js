5e42fa1051f26e3671492b31f5a53cd583e8088d
men21049 <men21049@byui.edu>
2026-01-09 23:40:10 -0600

    w2

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,11 @@
 const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+const navigation = document.querySelector('nav ul');
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+
+	console.log('clicked ham');
 });
 
 let oLastModif = new Date(document.lastModified);
